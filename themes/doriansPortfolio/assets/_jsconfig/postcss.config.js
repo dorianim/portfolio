9e7eefbfea9379710a5c2940cf04e4ac1226c58a
Dorian Zedler <dorian@itsblue.de>
2021-10-12 09:19:45 +0200

    Try to fix npm issues

--- themes/doriansPortfolio/assets/_jsconfig/postcss.config.js
+++ themes/doriansPortfolio/assets/_jsconfig/postcss.config.js
@@ -4,8 +4,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   // see https://gohugo.io/hugo-pipes/postprocess/#css-purging-with-postcss
   content: [
     "./hugo_stats.json",
-    themeDir + "/hugo_stats.json",
-    "exampleSite/hugo_stats.json",
+    themeDir + "/hugo_stats.json"
   ],
   defaultExtractor: (content) => {
     let els = JSON.parse(content).htmlElements;
@@ -18,7 +17,7 @@ module.exports = {
     require("postcss-import")({
       path: [themeDir],
     }),
-    require("tailwindcss")(themeDir + "assets/css/tailwind.config.js"),
+    require("tailwindcss")(themeDir + "assets/_jsconfig/tailwind.config.js"),
     require("autoprefixer")({
       path: [themeDir],
     }),
