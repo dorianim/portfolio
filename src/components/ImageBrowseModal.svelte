c83e426e4d624f247a3e036dd19fd40fb68bf298
Dorian Zedler <dorian@itsblue.de>
2023-09-05 10:43:37 +0200

    Feat: rework gallery modal

--- src/components/ImageBrowseModal.svelte
+++ src/components/ImageBrowseModal.svelte
@@ -14,11 +14,18 @@
 	import ChevronLeft from 'svelte-material-icons/ChevronLeft.svelte';
 	import ChevronRight from 'svelte-material-icons/ChevronRight.svelte';
 	import { mod } from '$lib/mod';
+	import { fade } from 'svelte/transition';
 
 	export let images: Image[];
 	export let showImage: number | undefined;
 
 	let dialog: HTMLDialogElement;
+
+	const handleKeydown = (e: KeyboardEvent) => {
+		if (e.key === 'Escape') {
+			showImage = undefined;
+		}
+	};
 
 	$: {
 		if (!dialog) break $;
@@ -27,87 +34,93 @@
 	}
 </script>
 
-<!-- svelte-ignore a11y-click-events-have-key-events a11y-no-noninteractive-element-interactions -->
-<dialog
-	class="bg-transparent overflow-visible"
-	bind:this={dialog}
-	on:close={() => (showImage = undefined)}
-	on:click|self={() => dialog.close()}
->
-	<div class="h-[80vh] w-[80vw] flex flex-col items-center justify-center gap-4 overflow-visible">
-		{#if showImage !== undefined}
-			<span class="flex flex-col md:flex-row gap-2 items-center text-white">
-				{images[showImage].description}
-				<span class="flex flex-row items-center text-sm">
-					<Copyright />
-					{images[showImage].copyright}</span
+{#if showImage !== undefined}
+	<div
+		transition:fade={{ duration: 200 }}
+		class="fixed left-0 top-0 z-[1001] grid h-screen w-screen grid-cols-4 grid-rows-[64px_1fr] overflow-y-hidden bg-black"
+	>
+		<!-- header -->
+		<div class="z-[1000] col-span-4 col-start-1 row-span-1 row-start-1 transition-transform">
+			<div
+				class="z-[1001] grid grid-cols-1 grid-rows-1 h-16 bg-gradient-to-b from-black/60 transition-transform duration-200"
+			>
+				<div
+					class="justify-self-center col-start-1 h-16 flex flex-col pt-1 items-center text-white"
 				>
-			</span>
-			<img
-				class="max-h-[80%] max-w-full object-contain"
-				src={images[showImage].src}
-				alt="{images[showImage].description}, (C) {images[showImage].copyright}"
-			/>
-			<span class="text-white">
-				{showImage + 1} / {images.length}
-			</span>
-		{/if}
+					<span class="text-lg">
+						{images[showImage].description}
+					</span>
+					<span class="flex flex-row items-center text-sm">
+						<Copyright />
+						{images[showImage].copyright}</span
+					>
+				</div>
 
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
+				<button
+					class="justify-self-end col-start-1 mx-2 p-2 text-xl md:mx-4 md:p-3 md:text-4xl rounded-full bg-[#0000007f] transition hover:bg-gray-500 text-white"
+					on:click={() => (showImage = undefined)}
+				>
+					<Close />
+				</button>
+			</div>
+		</div>
+
+		<!-- svelte-ignore a11y-no-static-element-interactions a11y-click-events-have-key-events -->
 		<div
-			class="absolute top-[50%] left-0 translate-y-[-50%] translate-x-[-9vw] h-[100%] w-[50%] text-white text-4xl cursor-pointer"
+			class="column-span-1 z-[999] col-start-1 row-span-1 row-start-2 mb-[60px] justify-self-start"
 			on:click={() => (showImage = mod((showImage ?? 0) - 1, images.length))}
 		>
-			<ChevronLeft class="absolute left-0 top-[50%] translate-y-[-50%]" />
+			<div class="group flex h-full place-items-center">
+				<button
+					class="mx-2 p-2 text-xl md:mx-4 md:p-3 md:text-4xl rounded-full bg-[#0000007f] transition group-hover:bg-gray-500 text-white"
+				>
+					<ChevronLeft class="" />
+				</button>
+			</div>
 		</div>
 
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
+		{#if showImage !== undefined}
+			<div class="col-span-4 col-start-1 row-span-3 row-start-1">
+				<div class="flex h-full select-none place-content-center place-items-center">
+					<div class="h-full w-full">
+						<img
+							class="h-full w-full object-contain"
+							src={images[showImage].src}
+							alt="{images[showImage].description}, (C) {images[showImage].copyright}"
+						/>
+					</div>
+				</div>
+			</div>
+		{/if}
+
+		<!-- svelte-ignore a11y-no-static-element-interactions a11y-click-events-have-key-events -->
 		<div
-			class="absolute top-[50%] right-0 translate-y-[-50%] translate-x-[9vw] h-[100%] w-[50%] text-white text-4xl cursor-pointer"
+			class="z-[999] col-span-1 col-start-4 row-span-1 row-start-2 mb-[60px] justify-self-end"
 			on:click={() => (showImage = mod((showImage ?? 0) + 1, images.length))}
 		>
-			<ChevronRight class="absolute right-0 top-[50%] translate-y-[-50%]" />
+			<div class="group flex h-full place-items-center">
+				<button
+					class="mx-2 p-2 text-xl md:mx-4 md:p-3 md:text-4xl rounded-full bg-[#0000007f] transition group-hover:bg-gray-500 text-white"
+				>
+					<ChevronRight class="" />
+				</button>
+			</div>
 		</div>
 
-		<!-- svelte-ignore a11y-no-static-element-interactions -->
-		<div
-			class="absolute top-0 right-0 text-white text-4xl cursor-pointer"
-			on:click={() => (showImage = undefined)}
-		>
-			<Close />
+		<!-- footer -->
+		<div class="z-[1000] col-span-4 col-start-1 row-span-1 row-start-3 transition-transform">
+			<div
+				class="z-[1001] flex h-16 place-items-center justify-center bg-gradient-to-t from-black/40 transition-transform duration-200"
+			>
+				<span class="text-white text-lg">
+					{showImage + 1} / {images.length}
+				</span>
+			</div>
 		</div>
 	</div>
-</dialog>
+{/if}
+
+<svelte:window on:keydown={handleKeydown} />
 
 <style>
-	dialog {
-		border-radius: 0.2em;
-		border: none;
-		padding: 0;
-	}
-	dialog::backdrop {
-		background: rgba(0, 0, 0, 0.5);
-	}
-	dialog[open] {
-		animation: zoom 0.3s cubic-bezier(0.34, 1.56, 0.64, 1);
-	}
-	@keyframes zoom {
-		from {
-			transform: scale(0.95);
-		}
-		to {
-			transform: scale(1);
-		}
-	}
-	dialog[open]::backdrop {
-		animation: fade 0.3s ease-out;
-	}
-	@keyframes fade {
-		from {
-			opacity: 0;
-		}
-		to {
-			opacity: 1;
-		}
-	}
 </style>
