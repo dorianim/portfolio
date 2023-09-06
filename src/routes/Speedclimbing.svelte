67cbc27bb040e128fada333f742bf75c6000fea6
Dorian Zedler <dorian@itsblue.de>
2023-09-06 11:12:36 +0200

    Chore: improove text

--- src/routes/Speedclimbing.svelte
+++ src/routes/Speedclimbing.svelte
@@ -186,29 +186,31 @@
 
 <div class="w-full flex flex-col xl:flex-row gap-10 justify-between">
 	<div class="flex flex-col gap-2">
-		<span class="text-2xl font-bold text-white">My story...</span>
+		<span class="text-2xl font-bold text-white">My journey...</span>
 		<p class="">
-			When I was four years old, my Dad took me to the climbing gym for the first time. I was
-			immediately fascinated by the sport and started climbing regularly. At the age of 5, I joined
-			the <a class="text-blue-500" href="https://climbingteam.de">Climbingteam Stuttgart</a>
-			and started to compete in kids-cups, which was a combination of lead, bouldering and classic speed.
+			When I was just four years old, my dad introduced me to climbing at a local gym, and I was
+			immediately hooked. By the age of five, I joined the <a
+				class="text-blue-500"
+				href="https://climbingteam.de">Climbingteam Stuttgart</a
+			>
+			and started competing in kids' climbing events that included lead, bouldering, and classic speed.
 			<br />
-			As I grew older, I started to compete at state and later at national level, at the time, speed
-			was not really a thing yet, so I competed in lead and bouldering.
+			As I got older, I began competing at the state and then national level, mainly in lead and bouldering
+			since speed climbing wasn't as popular back then.
 			<br />
-			When I was about 15 years old, I touched the normspeed wall for the first time. It was love at
-			first sight. I started to improove rapidly and won a bronze medal on my first national speed youth
-			cup in 2017.
+			Around the age of 15, I tried speed climbing for the first time and fell in love with it. I improved
+			quickly and even won a bronze medal in my first national speed youth cup in 2017.
 			<br />
-			After that, I still competed in lead and bouldering, but speed was slowly starting to become my
-			main focus. In 2020 I ultimately decided to compeletely focus on speed. In 2021, I was able to
-			qualify for my first international speed competition, the European Youth Cup in Bochum, where I
-			placed 4th.
+			While I still participated in lead and bouldering, I gradually shifted my focus towards speed climbing.
+			In 2020, I decided to fully commit to it. The following year, I achieved a big milestone by qualifying
+			for my first international speed competition, the European Youth Cup in Bochum, where I came in
+			fourth.
 			<br />
-			Since then, there have been lots of ups and downs, but overall I am making good progress and was
-			able to accomplish my big dream of competing in a worldcup in 2023.
+			Since then, I've had my share of ups and downs, but I'm making good progress. In 2023, I reached
+			another major milestone by competing in a World Cup event.
 			<br />
-			Wanna know how the story goes on? Follow me on Instagram to stay up to date!
+			If you're curious about what comes next, you can follow my journey on Instagram or Mastodon to
+			stay in the loop.
 		</p>
 		<span class="text-2xl font-bold text-white">In the press</span>
 		<div class="grid grid-cols-1 gap-3">
